60756b64ece338530f4ef3f6185d1f2bf000aa4a
Hougo <hougo13@gmail.com>
2018-10-04 18:09:05 +0200

    create modules and graphql schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ConfigModule } from './config/config.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { LibraryModule } from './library/library.module';
+import { MovieModule } from './movie/movie.module';
+import { AuthModule } from './auth/auth.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
   imports: [
@@ -10,6 +15,11 @@ import { PrismaModule } from './prisma/prisma.module';
       typePaths: ['./**/*.graphql'],
     }),
     PrismaModule,
+    UserModule,
+    LibraryModule,
+    MovieModule,
+    AuthModule,
+    RoomModule,
   ],
   controllers: [],
   providers: [],
